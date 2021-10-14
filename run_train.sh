1c5439c04f6500b5c8b73df4913cc2c9aa1b3aa0
Manor Zvi <manor.zvi@campus.technion.ac.il>
2021-10-14 12:35:49 +0300

    .

--- run_train.sh
+++ run_train.sh
@@ -1,55 +1,58 @@
 
-log_prefix=log_shapenet
-dump_prefix=dump_shapenet
-eval_prefix=eval_shapenet
-suffix=''
+x=_shapenet_cond_votes
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir $log_prefix --dump_results --dump_dir $dump_prefix
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir $log_prefix --dump_results --dump_dir $dump_prefix
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}/checkpoint.tar --dump_dir $eval_prefix --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}/checkpoint.tar --dump_dir $eval_prefix --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+x=${x}_neg_votes
+y=${x}_cond_bboxs
 
-suffix=${suffix}_cond_votes
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${y} --dump_results --dump_dir dump${y} --use_cond_votes --use_cond_bboxs --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${y} --dump_results --dump_dir dump${y} --use_cond_votes --use_cond_bboxs --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${y}/checkpoint.tar --dump_dir eval${y} --use_cond_votes --use_cond_bboxs --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${y}/checkpoint.tar --dump_dir eval${y} --use_cond_votes --use_cond_bboxs --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+x=${x}_cond_bboxs
 
-suffix=${suffix}_rand_votes
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_rand_votes
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_rand_votes
 
-echo python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_rand_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_rand_votes --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-log_prefix=${log_prefix}_w_two_backbones
-dump_prefix=${dump_prefix}_w_two_backbones
-eval_prefix=${eval_prefix}_w_two_backbones
-suffix=''
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir $log_prefix --dump_results --dump_dir $dump_prefix --use_two_backbones
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir $log_prefix --dump_results --dump_dir $dump_prefix --use_two_backbones
 
-echo python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}/checkpoint.tar --dump_dir ${eval_prefix} --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py  --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}/checkpoint.tar --dump_dir ${eval_prefix} --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+x=_shapenet_w_two_backbones_cond_votes
 
-suffix=${suffix}_cond_votes
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_two_backbones --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_two_backbones --max_epoch 80
+     echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+          python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_two_backbones
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_two_backbones
+x=${x}_neg_votes
+y=${x}_cond_bboxs
 
-echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_two_backbones --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_two_backbones --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-suffix=${suffix}_rand_votes
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${y} --dump_results --dump_dir dump${y} --use_cond_votes --use_cond_bboxs --use_two_backbones --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${y} --dump_results --dump_dir dump${y} --use_cond_votes --use_cond_bboxs --use_two_backbones --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${y}/checkpoint.tar --dump_dir eval${y} --use_cond_votes --use_cond_bboxs --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${y}/checkpoint.tar --dump_dir eval${y} --use_cond_votes --use_cond_bboxs --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
 
-echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_rand_votes --use_two_backbones
-CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir ${log_prefix}${suffix} --dump_results --dump_dir ${dump_prefix}${suffix} --use_cond_votes --use_rand_votes --use_two_backbones
+x=${x}_cond_bboxs
 
-echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_rand_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-python eval.py --dataset shapenet --model cond_votenet --checkpoint_path ${log_prefix}${suffix}/checkpoint.tar --dump_dir ${eval_prefix}${suffix} --use_cond_votes --use_rand_votes --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
-
+echo CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --use_two_backbones --max_epoch 80
+     CUDA_VISIBLE_DEVICES=0 python train.py --dataset shapenet --model cond_votenet --log_dir log${x} --dump_results --dump_dir dump${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --use_two_backbones --max_epoch 80
+echo python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
+     python eval.py --dataset shapenet --model cond_votenet --checkpoint_path log${x}/checkpoint.tar --dump_dir eval${x} --use_cond_votes --use_neg_votes --use_cond_bboxs --use_two_backbones --cluster_sampling seed_fps --use_3d_nms --use_cls_nms --per_class_proposal
